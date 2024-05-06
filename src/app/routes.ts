5a45bdd5f29bf86c958c2045a39fa951d5d907de
Verda Muso <gearmobile@gmail.com>
2024-05-06 19:44:04 +0300

    add routing

---
+++ src/app/routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { MainPageComponent } from './shared/main-page/main-page.component';
+import { ProductPageComponent } from './shared/product-page/product-page.component';
+import { CartPageComponent } from './shared/cart-page/cart-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: MainPageComponent },
+      { path: 'product/:id', component: ProductPageComponent },
+      { path: 'cart', component: CartPageComponent },
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+    ],
+  },
+];
